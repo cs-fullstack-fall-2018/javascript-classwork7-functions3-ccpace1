70d90afe463b33148405c1c3ba84d0a10ed30867
Christopher Pace <Christopher_pace@ymail.com>
2018-07-31 14:27:22 -0500

    Commit of Ex18

---
+++ exercise18.js
@@ -0,0 +1,13 @@
+function main(){
+    var number1 = 10;
+    var number2 = 10;
+    alert("The product is " + product(number1, number2));
+    alert("The quotient is " + quotient(number1, number2));
+}
+function product(num1,num2) {
+    return num1*num2;
+}
+function quotient(num1,num2){
+    return num1/num2;
+}
+main();
