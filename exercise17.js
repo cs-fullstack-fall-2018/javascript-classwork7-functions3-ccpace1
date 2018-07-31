080554ce6a8fbe751d7b00f2fd19f7ab63b0f925
Christopher Pace <Christopher_pace@ymail.com>
2018-07-31 14:17:25 -0500

    Initial Commit

---
+++ exercise17.js
@@ -0,0 +1,12 @@
+function main(){
+    var number1 = 14;
+    var number2 = 15;
+    var number3 = 16;
+    alert(countingFunction(number1, number2, number3));
+
+}
+function countingFunction(num1, num2, num3) {
+    return (num1+num2+num3);
+}
+
+main();
